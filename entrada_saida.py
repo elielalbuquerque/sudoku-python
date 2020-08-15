10899e385fd1661eb882910c981d271859b020f3
Gustavo Zanatta Bruno <gkbsa2504@gmail.com>
2020-08-15 14:00:13 -0400

    Implemented test VFS

--- entrada_saida.py
+++ entrada_saida.py
@@ -58,37 +58,4 @@ def imprime_resultado(resultado, t, s=0):
         try:
             numpy.savetxt(arquivo_saida, resultado, delimiter=' ', fmt='%d')
         except (FileNotFoundError, IOError):
-            print("Arquivo de saída não encontrado!")
-
-def convert_txt_to_array_erase (arquivo):
-    try:
-        dados = open(arquivo, "r")
-    except (FileNotFoundError, IOError):
-        print("Arquivo de entrada não encontrado!")
-    quadro = numpy.zeros((9,9))
-    contador = 0
-    i = 0
-    j = 0
-    eNumero = False
-    while contador <81:
-        conteudo=dados.readline()
-        eNumero = False
-        for x in range(len(conteudo)):
-            try:
-                if int(conteudo[x]) > 0 and int(conteudo[x]) < 10:
-                    quadro[i,j]=int(conteudo[x])
-                    j = j + 1
-                    contador = contador +1
-                    eNumero = True
-            except ValueError:
-                if conteudo[x]=='_':
-                    j = j + 1
-                    contador = contador +1
-                    eNumero = True
-                #else:
-                    #print(content[x], 'is not regonized')
-            if j == 9:
-                j = 0
-        if eNumero:
-            i = i + 1
-    return quadro   
+            print("Arquivo de saída não encontrado!") 
--- tests.py
+++ tests.py
@@ -1,8 +1,62 @@
+import numpy, time
 from sudoku import Sudoku
-# Initializes a Sudoku puzzle with 3 x 3 sub-grid and
-# generates a puzzle with half of the cells empty
+from breadthFirstSearch.Sudoku_Player_BFS import SudokuPlayerBFS
+from aStar.Sudoku_A_Star import SudokuPlayerAStar
 
-def generate_tests():
-    for i in range(10):
-        puzzle = Sudoku(3).difficulty(0.9)
-        print (puzzle, file=open("tests/inputs.txt", "a"))
+
+def generate_inputs():
+    for i in range(100):
+        for j in range (1, 10):
+            difficulty_graduation = float(f'0.{j}')
+            board = Sudoku(3).difficulty(difficulty_graduation)
+            print (board, file=open(f"tests/inputs_{j}.txt", "a"))
+
+def solve_inputs():
+    for files_it in range (1,10):
+        print (f'Calculating file {files_it}')
+        try:
+            dados = open(f"tests/inputs_{files_it}.txt", "r")
+        except (FileNotFoundError, IOError):
+            print("Arquivo de entrada não encontrado!")
+        quadro = numpy.zeros((9,9))
+        contador = 0
+        i = 0
+        j = 0
+        eNumeroLinha = False
+        line_valid_places=[2,4,6,10,12,14,18,20,22]
+        for conteudo in dados:
+            eNumeroLinha = False
+            for x in range(len(conteudo)):
+                try:
+                    if int(conteudo[x]) > 0 and int(conteudo[x]) < 10:
+                        quadro[i,j]=int(conteudo[x])
+                        j = j + 1
+                        contador = contador +1
+                        eNumeroLinha = True
+                except ValueError:
+                    if (x in line_valid_places) and (conteudo[0])=='|':
+                        j = j + 1
+                        contador = contador +1
+                        eNumeroLinha = True
+                    #else:
+                        #print(conteudo[x], 'is not regonized')
+                if j == 9:
+                    j = 0    
+            if eNumeroLinha:
+                i = i + 1
+                eNumeroLinha = False
+            if i == 9:
+                print(quadro)
+                t1 = time.time()
+                sudoku_bfs = SudokuPlayerBFS(quadro.tolist())
+                result = numpy.asarray(sudoku_bfs.busca_em_largura(), dtype=numpy.int32)
+                s = sudoku_bfs.get_steps()
+                t = time.time() - t1
+                print(f'{t} {s}',file=open(f"tests/bfs_{files_it}.txt", "a"))
+                contador = 0
+                i = 0
+                j = 0
+                quadro = numpy.zeros((9,9))
+       
+
+solve_inputs()
